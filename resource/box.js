26d6aafc670490b6ebc14ebb4c085293eb04ed4a
jahid140191 <jahidhasan29976@gmail.com>
2022-11-19 23:11:11 +0600

    this is task5

---
+++ resource/box.js
@@ -0,0 +1,153 @@
+
+let div1=document.querySelector(".div1");
+let div2=document.querySelector(".div2");
+let div3=document.querySelector(".div3");
+let div4=document.querySelector(".div4");
+
+let div5=document.querySelector(".div5");
+let div6=document.querySelector(".div6");
+let div7=document.querySelector(".div7");
+let div8=document.querySelector(".div8");
+
+let div9=document.querySelector(".div9");
+let div10=document.querySelector(".div10");
+let div11=document.querySelector(".div11");
+let div12=document.querySelector(".div12");
+
+let div13=document.querySelector(".div13");
+let div14=document.querySelector(".div14");
+let div15=document.querySelector(".div15");
+let div16=document.querySelector(".div16");
+
+
+
+
+
+// first column
+var count=1;
+
+let udiv= document.querySelector(`.div${count}`);
+
+
+
+    div1.addEventListener('click' ,function Div1(){
+        if(div1.innerHTML.length==0){
+            div1.innerHTML =  count;
+            count++;
+        }
+
+    })
+
+    div2.addEventListener('click' ,function Div2(){
+        if(div2.innerHTML.length==0){
+        div2.innerHTML =  count;
+        count++;
+        }
+    })
+
+    div3.addEventListener('click' ,function Div3(){
+        if(div3.innerHTML.length===0){
+        div3.innerHTML =  count;
+        count++;
+        }
+    })
+    div4.addEventListener('click' ,function Div4(){
+        if(div4.innerHTML.length===0){
+        div4.innerHTML =  count;
+        count++;
+        }
+    })
+
+
+
+// second column
+    div5.addEventListener('click' ,function Div5(){
+        if(div5.innerHTML.length==0){
+            div5.innerHTML =  count;
+            count++;
+        }
+        
+    })
+    div6.addEventListener('click' ,function Div6(){
+        if(div6.innerHTML.length==0){
+        div6.innerHTML =  count;
+        count++;
+        }
+    })
+    div7.addEventListener('click' ,function Div7(){
+        if(div7.innerHTML.length===0){
+        div7.innerHTML =  count;
+        count++;
+        }
+    })
+    div8.addEventListener('click' ,function Div8(){
+        if(div8.innerHTML.length===0){
+        div8.innerHTML =  count;
+        count++;
+        }
+    })
+
+
+
+// third column
+    div9.addEventListener('click' ,function Div9(){
+        if(div9.innerHTML.length==0){
+            div9.innerHTML =  count;
+            count++;
+        }
+
+    })
+    div10.addEventListener('click' ,function Div10(){
+        if(div10.innerHTML.length==0){
+        div10.innerHTML =  count;
+        count++;
+        }
+    })
+    div11.addEventListener('click' ,function Div11(){
+        if(div11.innerHTML.length===0){
+        div11.innerHTML =  count;
+        count++;
+        }
+    })
+    div12.addEventListener('click' ,function Div12(){
+        if(div12.innerHTML.length===0){
+        div12.innerHTML =  count;
+        count++;
+        }
+    })
+
+
+
+// fourth column
+    div13.addEventListener('click' ,function Div13(){
+        if(div13.innerHTML.length==0){
+            div13.innerHTML =  count;
+            count++;
+        }
+
+    })
+    div14.addEventListener('click' ,function Div14(){
+        if(div14.innerHTML.length==0){
+        div14.innerHTML =  count;
+        count++;
+        }
+    })
+    div15.addEventListener('click' ,function Div15(){
+        if(div15.innerHTML.length===0){
+        div15.innerHTML =  count;
+        count++;
+        }
+    })
+    div16.addEventListener('click' ,function Div16(){
+        if(div16.innerHTML.length===0){
+        div16.innerHTML =  count;
+        count++;
+        }
+    })
+
+   
+    // udiv.style.background="red";
+
+    
+    
+    
